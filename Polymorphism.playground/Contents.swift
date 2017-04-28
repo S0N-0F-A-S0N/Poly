902e5d42d49ca2bd02417647f28a8874b0e0ec75
S0N <senpai@Elijahs-MacBook-Pro.local>
2017-04-27 23:15:07 -0500

    Second

---
+++ Polymorphism.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+class Shape {
+    var area: Double?
+    
+    func calculateArea(valueA: Double, valueB: Double) {
+        
+    }
+}
+
+class Triangle: Shape {
+    
+    override func calculateArea(valueA: Double, valueB: Double) {
+        
+        area = (valueA * valueB) / 2
+        
+    }
+    
+}
+
+class Rectangle: Shape {
+    
+    override func calculateArea(valueA: Double, valueB: Double) {
+        
+        area = valueA * valueB
+        
+    }
+    
+}
+
